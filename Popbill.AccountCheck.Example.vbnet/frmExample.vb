99a23dccf25ea37f71f53d054d0bf7d12626eaeb
JohnJeong123 <frenchofkiss@gmail.com>
2020-06-30 15:28:24 +0900

    added AccountCheckService API Example

---
+++ Popbill.AccountCheck.Example.vbnet/frmExample.vb
@@ -0,0 +1,387 @@
+﻿
+'=========================================================================
+'
+' 팝빌 예금주조회 API VB.Net SDK Example
+'
+' - 업데이트 일자 : 2020-06-30
+' - 연동 기술지원 연락처 : 1600-9854 / 070-4504-2991
+' - 연동 기술지원 이메일 : code@linkhub.co.kr
+'
+' <테스트 연동개발 준비사항>
+' 1) 19, 22번 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
+'    링크허브 가입시 메일로 발급받은 인증정보를 참조하여 변경합니다.
+' 2) 팝빌 개발용 사이트(test.popbill.com)에 연동회원으로 가입합니다.
+'=========================================================================
+
+
+Public Class frmExample
+
+    '링크아이디
+    Private LinkID As String = "TESTER"
+
+    '비밀키
+    Private SecretKey As String = "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I="
+
+    '휴폐업조회 서비스 변수 선언
+    Private accountCheckService As AccountCheckService
+
+
+    Private Sub frmExample_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        '예금주조회 서비스 객체 초기화
+        accountCheckService = New AccountCheckService(LinkID, SecretKey)
+
+        '연동환경 설정값 (True-개발용, False-상업용)
+        accountCheckService.IsTest = True
+
+        '인증토큰의 IP제한기능 사용여부, (True-권장)
+        accountCheckService.IPRestrictOnOff = True
+
+    End Sub
+
+    Private Sub btnGetBalance_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetBalance.Click
+
+        Try
+            Dim remainPoint As Double = accountCheckService.GetBalance(txtCorpNum.Text)
+
+            MsgBox("연동회원 잔여포인트 : " + remainPoint.ToString())
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnGetChargeURL_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetChargeURL.Click
+
+        Try
+            Dim url As String = accountCheckService.GetChargeURL(txtCorpNum.Text, txtUserId.Text)
+
+            MsgBox(url)
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnGetPartnerBalance_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetPartnerBalance.Click
+
+        Try
+            '파트너 포인트충전 URL
+            Dim TOGO As String = "CHRG"
+
+            Dim url As String = accountCheckService.GetPartnerURL(txtCorpNum.Text, TOGO)
+
+            MsgBox(url)
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnCheckIsMember_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCheckIsMember.Click
+        Try
+            Dim response As Response = accountCheckService.CheckIsMember(txtCorpNum.Text, LinkID)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub btnCheckID_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCheckID.Click
+        Try
+            Dim response As Response = accountCheckService.CheckID(txtCorpNum.Text)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnJoinMember_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnJoinMember.Click
+
+        Dim joinInfo As JoinForm = New JoinForm
+
+        '아이디, 6자이상 50자 미만
+        joinInfo.ID = "userid"
+
+        '비밀번호, 6자이상 20자 미만
+        joinInfo.PWD = "pwd_must_be_long_enough"
+
+        '링크아이디
+        joinInfo.LinkID = LinkID
+
+        '사업자번호 "-" 제외
+        joinInfo.CorpNum = "1231212312"
+
+        '대표자명 (최대 100자)
+        joinInfo.CEOName = "대표자성명"
+
+        '상호 (최대 200자)
+        joinInfo.CorpName = "상호"
+
+        '사업장 주소 (최대 300자)
+        joinInfo.Addr = "주소"
+
+        '업태 (최대 100자)
+        joinInfo.BizType = "업태"
+
+        '종목 (최대 100자)
+        joinInfo.BizClass = "종목"
+
+        '담당자 성명 (최대 100자)
+        joinInfo.ContactName = "담당자명"
+
+        '담당자 이메일 (최대 20자)
+        joinInfo.ContactEmail = "test@test.com"
+
+        '담당자 연락처 (최대 20자)
+        joinInfo.ContactTEL = "070-4304-2991"
+
+        '담당자 휴대폰번호 (최대 20자)
+        joinInfo.ContactHP = "010-111-222"
+
+        '담당자 팩스번호 (최대 20자)
+        joinInfo.ContactFAX = "02-6442-9700"
+
+        Try
+            Dim response As Response = accountCheckService.JoinMember(joinInfo)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnGetChargeInfo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetChargeInfo.Click
+
+        Try
+            Dim ChargeInfo As ChargeInfo = accountCheckService.GetChargeInfo(txtCorpNum.Text)
+
+            Dim tmp As String = "unitCost (조회단가) : " + ChargeInfo.unitCost + vbCrLf
+            tmp += "chargeMethod (과금유형) : " + ChargeInfo.chargeMethod + vbCrLf
+            tmp += "rateSystem (과금제도) : " + ChargeInfo.rateSystem + vbCrLf
+
+            MsgBox(tmp)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnUnitCost_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUnitCost.Click
+
+        Try
+            Dim unitCost As Single = accountCheckService.GetUnitCost(txtCorpNum.Text)
+
+            MsgBox("예금주조회 단가(unitCost) : " + unitCost.ToString())
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnGetPartnerURL_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetPartnerURL.Click
+        Try
+            '파트너 포인트충전 URL
+            Dim TOGO As String = "CHRG"
+
+            Dim url As String = accountCheckService.GetPartnerURL(txtCorpNum.Text, TOGO)
+
+            MsgBox(url)
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnGetAccessURL_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetAccessURL.Click
+
+        Try
+            Dim url As String = accountCheckService.GetAccessURL(txtCorpNum.Text, txtUserId.Text)
+
+            MsgBox(url)
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnRegistContact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRegistContact.Click
+
+        '담당자 정보객체
+        Dim joinData As New Contact
+
+        '아이디 (6자이상 50자미만)
+        joinData.id = "testkorea1120"
+
+        '비밀번호 (6자이상 20자미만)
+        joinData.pwd = "password"
+
+        '담당자 성명 (최대 100자)
+        joinData.personName = "담당자명"
+
+        '담당자 연락처 (최대 20자)
+        joinData.tel = "070-1111-2222"
+
+        '담당자 휴대폰 (최대 20자)
+        joinData.hp = "010-1234-1234"
+
+        '담당자 팩스 (최대 20자)
+        joinData.fax = "070-1234-1234"
+
+        '담당자 이메일 (최대 100자)
+        joinData.email = "test@test.com"
+
+        '회사조회 권한여부, True-회사조회, False-개인조회
+        joinData.searchAllAllowYN = False
+
+        '관리자 여부, True-관리자, False-사용자
+        joinData.mgrYN = False
+
+        Try
+            Dim response As Response = accountCheckService.RegistContact(txtCorpNum.Text, joinData, txtUserId.Text)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnListContact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnListContact.Click
+
+        Try
+            Dim contactList As List(Of Contact) = accountCheckService.ListContact(txtCorpNum.Text, txtUserId.Text)
+
+            Dim tmp As String = "id(아이디) | personName(담당자명) | email(메일주소) | hp(휴대폰번호) | fax(팩스) | tel(연락처) |"
+            tmp += "regDT(등록일시) | searchAllAllowYN(회사조회 여부) | mgrYN(관리자 여부) | state(상태)" + vbCrLf
+
+            For Each info As Contact In contactList
+                tmp += info.id + " | " + info.personName + " | " + info.email + " | " + info.hp + " | " + info.fax + " | " + info.tel + " | "
+                tmp += info.regDT.ToString() + " | " + info.searchAllAllowYN.ToString() + " | " + info.mgrYN.ToString() + " | " + info.state + vbCrLf
+            Next
+
+            MsgBox(tmp)
+        Catch ex As PopbillException
+
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnUpdateContact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdateContact.Click
+
+        '담당자 정보객체
+        Dim joinData As New Contact
+
+        '아이디 (6자이상 50자미만)
+        joinData.id = "testkorea1120"
+
+        '담당자 성명 (최대 100자)
+        joinData.personName = "담당자명"
+
+        '담당자 연락처 (최대 20자)
+        joinData.tel = "070-1111-2222"
+
+        '담당자 휴대폰 (최대 20자)
+        joinData.hp = "010-1234-1234"
+
+        '담당자 팩스 (최대 20자)
+        joinData.fax = "070-1234-1234"
+
+        '담당자 이메일 (최대 100자)
+        joinData.email = "test@test.com"
+
+        '회사조회 권한여부, True-회사조회, False-개인조회
+        joinData.searchAllAllowYN = False
+
+        '관리자 여부, True-관리자, False-사용자
+        joinData.mgrYN = False
+
+        Try
+            Dim response As Response = accountCheckService.UpdateContact(txtCorpNum.Text, joinData, txtUserId.Text)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnGetCorpInfo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGetCorpInfo.Click
+        Try
+            Dim corpInfo As CorpInfo = accountCheckService.GetCorpInfo(txtCorpNum.Text, txtUserId.Text)
+
+            Dim tmp As String = "ceoname(대표자성명) : " + corpInfo.ceoname + vbCrLf
+            tmp += "corpName(상호) : " + corpInfo.corpName + vbCrLf
+            tmp += "addr(주소) : " + corpInfo.addr + vbCrLf
+            tmp += "bizType(업태) : " + corpInfo.bizType + vbCrLf
+            tmp += "bizClass(종목) : " + corpInfo.bizClass + vbCrLf
+
+            MsgBox(tmp)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnUpdateCorpInfo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdateCorpInfo.Click
+
+        Dim corpInfo As New CorpInfo
+
+        '대표자명(최대 100자)
+        corpInfo.ceoname = "대표자명_수정"
+
+        '상호(최대 200자)
+        corpInfo.corpName = "상호_수정"
+
+        '주소(최대 300자)
+        corpInfo.addr = "주소_수정"
+
+        '업태(최대 100자)
+        corpInfo.bizType = "업태_수정"
+
+        '종목(최대 100자)
+        corpInfo.bizClass = "종목_수정"
+
+        Try
+
+            Dim response As Response = accountCheckService.UpdateCorpInfo(txtCorpNum.Text, corpInfo, txtUserId.Text)
+
+            MsgBox("응답코드(code) : " + response.code.ToString() + vbCrLf + "응답메시지(message) : " + response.message)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnCheckCorpNum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCheckCorpNum.Click
+
+        Try
+            Dim accountInfo As AccountCheckInfo = accountCheckService.CheckAccountInfo(txtCorpNum.Text, txtBankCode.Text, txtAccountNumber.Text)
+
+            Dim tmp As String = ""
+
+            tmp += "bankCode (기관코드) : " + accountInfo.bankCode + vbCrLf
+            tmp += "accountNumber (계좌번호) : " + accountInfo.accountNumber + vbCrLf
+            tmp += "accountName (예금주 성명) : " + accountInfo.accountName + vbCrLf
+            tmp += "checkDate (확인일시) : " + accountInfo.checkDate + vbCrLf
+            tmp += "resultCode (응답코드) : " + accountInfo.resultCode + vbCrLf
+            tmp += "resultMessage (응답메시지) : " + accountInfo.resultMessage + vbCrLf
+
+            MsgBox(tmp)
+
+        Catch ex As PopbillException
+            MsgBox("응답코드(code) : " + ex.code.ToString() + vbCrLf + "응답메시지(message) : " + ex.Message)
+        End Try
+    End Sub
+End Class
