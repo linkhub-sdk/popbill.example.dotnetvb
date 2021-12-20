930d7b8d41d92d9d5a8f2eea03aaca647a724f17
ProWON <pro_won45@naver.com>
2021-12-20 10:43:21 +0900

    fixed Update data

--- Popbill.Fax.Example.vbnet/frmExample.vb
+++ Popbill.Fax.Example.vbnet/frmExample.vb
@@ -3,7 +3,7 @@
 ' 팝빌 팩스 API VB.Net SDK Example
 '
 '' - VB.Net SDK 연동환경 설정방법 안내 : https://docs.popbill.com/fax/tutorial/dotnet#vb
-' - 업데이트 일자 : 2021-08-05
+' - 업데이트 일자 : 2021-12-20
 ' - 연동 기술지원 연락처 : 1600-9854 / 070-4304-2991
 ' - 연동 기술지원 이메일 : code@linkhub.co.kr
 '
