d87abf3d82cfecdaea801b3cfbb4fd986b55bc3f
ProWON <pro_won45@naver.com>
2021-12-23 17:00:16 +0900

    fixed email address, removed tel

--- Popbill.Hometax.Cashbill.Example.vbnet/frmExample.vb
+++ Popbill.Hometax.Cashbill.Example.vbnet/frmExample.vb
@@ -4,8 +4,8 @@
 '
 ' - VB.Net SDK 연동환경 설정방법 안내 : https://docs.popbill.com/htcashbill/tutorial/dotnet#vb
 ' - 업데이트 일자 : 2021-08-05
-' - 연동 기술지원 연락처 : 1600-8536 / 070-4304-2991
-' - 연동 기술지원 이메일 : code@linkhub.co.kr
+' - 연동 기술지원 연락처 : 1600-8536
+' - 연동 기술지원 이메일 : code@linkhubcorp.com
 '
 ' <테스트 연동개발 준비사항>
 ' 1) 23, 26번 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
