173978f33c4da61ba1c7baeda32b9c5a483ba4f5
AlBan93 <albaneo0724@gmail.com>
2023-07-03 15:36:06 +0900

    업데이트 일자 변경

--- Popbill.Kakao.Example.vbnet/frmExample.vb
+++ Popbill.Kakao.Example.vbnet/frmExample.vb
@@ -3,7 +3,7 @@
 ' 팝빌 카카오톡 API VB.Net SDK Example
 '
 ' - VB.Net 연동환경 설정방법 안내 : https://developers.popbill.com/guide/kakaotalk/dotnet/getting-started/tutorial?fwn=vb
-' - 업데이트 일자 : 2023-06-11
+' - 업데이트 일자 : 2023-07-03
 ' - 연동 기술지원 연락처 : 1600-9854
 ' - 연동 기술지원 이메일 : code@linkhubcorp.com
 '
--- Popbill.Taxinvoice.Example.vbnet/frmExample.vb
+++ Popbill.Taxinvoice.Example.vbnet/frmExample.vb
@@ -3,7 +3,7 @@
 ' 팝빌 전자세금계산서 API VB.Net SDK Example
 '
 ' - VB.NET SDK 연동환경 설정방법 안내 : https://developers.popbill.com/guide/taxinvoice/dotnet/getting-started/tutorial?fwn=vb
-' - 업데이트 일자 : 2023-06-11
+' - 업데이트 일자 : 2023-07-03
 ' - 연동 기술지원 연락처 : 1600-9854
 ' - 연동 기술지원 이메일 : code@linkhubcorp.com
 '
