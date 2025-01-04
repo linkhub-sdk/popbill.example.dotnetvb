318287ce19dc0e752bdc50cd50c29cd07bd465a6
정우석 <wsj@linkhubcorp.com>
2025-01-04 18:06:47 +0900

    V24-11-07 수정

--- Popbill.Taxinvoice.Example.vbnet/frmExample.vb
+++ Popbill.Taxinvoice.Example.vbnet/frmExample.vb
@@ -2,7 +2,7 @@
 ' 팝빌 전자세금계산서 API .NET SDK VB.NET Example
 ' VB.NET 연동 튜토리얼 안내 : https://developers.popbill.com/guide/taxinvoice/dotnet/getting-started/tutorial?fwn=vb
 '
-' 업데이트 일자 : 2024-11-12
+' 업데이트 일자 : 2025-01-04
 ' 연동기술지원 연락처 : 1600-9854
 ' 연동기술지원 이메일 : code@linkhubcorp.com
 '         
